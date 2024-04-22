616ddb1ff6cc7a0df78edac625631c4608098340
Ultra-pn4rv <tsarultra@gmail.com>
2024-04-22 08:15:59 -0400

    questao 3

---
+++ questao3.js
@@ -0,0 +1,9 @@
+/*
+Questão 3: Escreva um código JavaScript utilizando o operador ternário para 
+determinar se uma pessoa pode dirigir ou não. A variável idade contém a idade 
+da pessoa. Atribua o resultado à variável permissao.
+*/
+
+let idade = 28
+let sla = (idade>= 18) ? "vc pode dirigir" : "vc é menor de idade, sai daí!";
+console.log(sla);
